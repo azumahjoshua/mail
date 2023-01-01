14854bbe1bad605bbff2827c1939ecf1d4d0c727
azumahjoshua <joshuaazumah1086@gmail.com>
2023-01-01 21:28:45 +0100

    add mail load function

---
+++ README.md
@@ -0,0 +1,3 @@
+## cs50-mail project 3
+
+Design a front-end for an email client that makes API calls to send and receive emails.
--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose-form').addEventListener('submit',send_mail)
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -15,6 +16,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector("#single-email-view").style.display ='none'
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -31,3 +33,33 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
+//function to load eamil when it's sent
+function load_email(emaildata, origin_mailbox) {
+  document.querySelector('#emails-view').style.display = 'none'
+  document.querySelector('#compose-view').style.display = 'none'
+  document.querySelector('#email-view').style.display = 'block'
+    
+  
+}
+function send_mail() {
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+       recipients: recipients,
+        subject: subject,
+        body: body
+    })
+  })
+    .then((response) => response.json())
+    .then(result => {
+      load_mailbox('sent')
+    })
+    .catch(error => {
+     console.log(error);
+  })
+  return false
+}
+
